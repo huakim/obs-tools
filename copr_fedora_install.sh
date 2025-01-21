1aa57346104576c109511fc6678e7da4d3b990c3
huakim tylyktar <zuhhaga@gmail.com>
2025-01-21 16:57:29 +0000

    Update copr_fedora_install.sh

--- copr_fedora_install.sh
+++ copr_fedora_install.sh
@@ -2,6 +2,5 @@ temp_dir="$(mktemp -d)"
 mkdir "${temp_dir}/SOURCES"
 dnf install -y sed perl-generators rpm-build
 cp * "${temp_dir}/SOURCES/"
-#"-DONLY_PACKAGE_MANAGERS $(basename $(realpath $(which dnf)))"
-rpmbuild "-DNO_COPR_TOOLS yes" "-D_topdir ${temp_dir}" --bb "${temp_dir}/SOURCES/obs-tools.spec"
+rpmbuild "-DONLY_PACKAGE_MANAGERS $(basename $(realpath $(which dnf)))" "-DNO_COPR_TOOLS yes" "-D_topdir ${temp_dir}" --bb "${temp_dir}/SOURCES/obs-tools.spec"
 dnf install -y "${temp_dir}/RPMS"/noarch/*.noarch.rpm
